08f3c45b8f5c96c98a54478095f994a569884a2c
Leman <leman@Leman-MacBook-Air.local>
2023-05-29 17:56:14 +0400

    about page is done

---
+++ assets/scripts/about.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $('.logo-area').slick({
+dots: true,
+infinite: true,
+speed: 300,
+slidesToShow: 5,
+slidesToScroll: 1,
+centerMode:false,     
+    draggable:true,    
+    arrows:false,    
+responsive: [
+{
+  breakpoint: 1024,
+  settings: {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true
+  }
+},
+{
+  breakpoint: 600,
+  settings: {
+    slidesToShow: 2,
+    slidesToScroll: 1
+  }
+},
+{
+  breakpoint: 480,
+  settings: {
+    slidesToShow: 2,
+    slidesToScroll: 1
+  }
+}
+]
+});
+})
