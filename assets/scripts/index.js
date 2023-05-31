0345b87706505d88feee5d9f27522ebf7382edf5
Leman <leman@Leman-MacBook-Air.local>
2023-05-31 09:13:11 +0400

    index.html section 2 is done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -98,3 +98,63 @@ slideContainer.addEventListener('mousedown', dragStart);
 slideContainer.addEventListener('mousemove', dragMove);
 slideContainer.addEventListener('mouseup', dragEnd);
 slideContainer.addEventListener('mouseleave', dragEnd);
+
+
+// Function to start the countdown
+function startCountdown() {
+  // Set the initial countdown values
+  var days = 15;
+  var hours = 0;
+  var minutes = 0;
+  var seconds = 0;
+
+  // Function to update the countdown
+  function updateCountdown() {
+      // Decrease the seconds by 1
+      seconds--;
+
+      // Check if seconds reach 0
+      if (seconds < 0) {
+          seconds = 59;
+          minutes--;
+
+          // Check if minutes reach 0
+          if (minutes < 0) {
+              minutes = 59;
+              hours--;
+
+              // Check if hours reach 0
+              if (hours < 0) {
+                  hours = 23;
+                  days--;
+
+                  // Check if days reach 0
+                  if (days < 0) {
+                      clearInterval(countdownInterval);
+                      // Countdown has ended, do something here
+                  }
+              }
+          }
+      }
+
+      // Update the countdown elements in the HTML
+      document.getElementById("days").textContent = formatTime(days);
+      document.getElementById("hours").textContent = formatTime(hours);
+      document.getElementById("minutes").textContent = formatTime(minutes);
+      document.getElementById("seconds").textContent = formatTime(seconds);
+  }
+
+  // Function to format time values (add leading zeros)
+  function formatTime(time) {
+      return time < 10 ? "0" + time : time;
+  }
+
+  // Update the countdown immediately
+  updateCountdown();
+
+  // Update the countdown every second
+  var countdownInterval = setInterval(updateCountdown, 1000);
+}
+
+// Call the startCountdown function when the page is loaded
+window.addEventListener("load", startCountdown);
